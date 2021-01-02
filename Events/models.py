d3727177f5ec9fb4a0f683e31bc5bf2b51e8996f
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-02 14:48:48 +0530

    Normal float changed to djmoney for more flexiblity

--- Events/models.py
+++ Events/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 # Create your models here.
 
 class Event(models.Model):
     #id is automatically generated for each Instance by django
     event_name = models.CharField(max_length=30, blank=False) 
-    registration_fees = models.FloatField(blank=False) 
+    #registration_fees = models.FloatField(blank=False) 
+    registration_fees = MoneyField(max_digits=14, decimal_places=2, default_currency='INR')
+
+
     event_categories = models.CharField(max_length=100)
     event_description = models.CharField(max_length=250)
     event_contact = models.CharField(max_length=15)
