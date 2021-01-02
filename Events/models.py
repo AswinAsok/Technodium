b3e57200a93640d9070e51c9ea7a5c314a50ef04
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-02 14:40:23 +0530

    Time removed form models and timezone change to Asia/Kolkata

--- Events/models.py
+++ Events/models.py
@@ -9,8 +9,8 @@ class Event(models.Model):
     event_categories = models.CharField(max_length=100)
     event_description = models.CharField(max_length=250)
     event_contact = models.CharField(max_length=15)
-    event_start_time = models.DateTimeField(blank=True, null=True)
-    event_end_time = models.DateTimeField(blank=True, null=True)
+    event_start_time = models.DateField(blank=True, null=True)
+    event_end_time = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.event_name
