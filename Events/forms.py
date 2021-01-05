fbaaf588c04ae54d64e8873e664ced96a24d3d33
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-05 13:57:13 +0530

    Django Forms added from Registrations

---
+++ Events/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Registration
+
+class RegistrationForm(forms.ModelForm):
+    
+
+    class Meta:
+        model = Registration
+        fields = ('full_name','email', 'institute_name','github_profile')
--- Events/views.py
+++ Events/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
+
 
 from .models import Event, Registration
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -13,9 +16,33 @@ def home(request):
     #return HttpResponse("This is home page")
 
 def register(request):    
-    Registrations = Registration.objects.all()
     context = {}
+    Registrations = Registration.objects.all()
     context['Registrations'] = Registrations
 
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+            
+            
+        else:
+            form = RegistrationForm()
+            context['form'] = form
+
+    else:
+        form = RegistrationForm(
+            initial={
+                'full_name': '',
+                'email': '',
+                'institute_name': '',
+                'github_profile': '',
+            }
+        )
+        context['form'] = form
     return render(request, 'register.html', context)
+    
+
 
